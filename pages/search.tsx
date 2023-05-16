173bcaa9d7deae65ef22c3c6d26b7576f9530823
Samuel Terefe <samuelterefe2@gmail.com>
2023-05-16 22:13:02 +0300

    updatting all search

--- pages/search.tsx
+++ pages/search.tsx
@@ -58,14 +58,14 @@ RTL
 
 <div className="bg-white dark:bg-gray-900">
     <div className="max-w-3xl px-6 py-16 mx-auto text-center">
-        <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">Want to see Back in action?</h1>
+        <h1 className="text-3xl font-semibold text-gray-800 dark:text-gray-100">Search News?</h1>
         <p className="max-w-md mx-auto mt-5 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, minus tempora nemo quos</p>
           <form onSubmit={handleSubmit}>
         <div className="flex flex-col mt-8 space-y-3 sm:space-y-0 sm:flex-row sm:justify-center sm:-mx-2">
             <input name="searchQuery"   placeholder="E.g. politics, sports, ..."  type="text" className="px-4 py-2 text-gray-700 bg-white border rounded-md sm:mx-2 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
 
             <button type="submit"  disabled={searchResultsLoading} className="px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-700 rounded-md sm:mx-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-                Get Started
+                search now
             </button>
         </div>
         </form>
