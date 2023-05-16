f1cc1bc783b1a6e96854f9f464ab362199daf2bf
Samuel Terefe <samuelterefe2@gmail.com>
2023-05-16 15:39:34 +0300

    fetchdata work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,40 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { json } from 'stream/consumers'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[],
+}
+
+export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
+  const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWS_API_KEY);
+  const newsResponse: NewsResponse = await response.json();
+  return {
+    props: { newsArticles: newsResponse.articles }
+  }
+  // let error go to 500 page
+}
+
+
+export default function BreakingNewsPage({ newsArticles }: BreakingNewsPageProps) {
   return (
+    <>
+    <Head>
+        <title key="title">Breaking News - NextJS News App</title>
+      </Head>
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+     
     >
-         <h1> Home page </h1>
+         <h1> News Page </h1>
+         <p>This page uses <strong>getServerSideProps</strong> to fetch data server-side on every request.
+          This allows search engines to crawl the page content and <strong>improves SEO</strong>.</p>
+          { JSON.stringify(newsArticles)  }
     </main>
+    </>
   )
 }
